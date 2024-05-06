89274a0ffe7a61875f55afb63fd15dd336f343f8
Md Abul Hassan <imabulhasan99@gmail.com>
2024-05-07 00:34:12 +0600

    code cleanup

--- app/Http/Requests/Withdraw/CreateRequest.php
+++ app/Http/Requests/Withdraw/CreateRequest.php
@@ -22,7 +22,7 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'amount' => ['required', 'numeric', 'min:0'],
+            'amount' => ['required', 'numeric', 'min:1'],
         ];
     }
 
