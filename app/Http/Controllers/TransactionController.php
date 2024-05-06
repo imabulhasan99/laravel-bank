45106748b35c8e2ea5f804fcb63ae57433aa8245
Md Abul Hassan <imabulhasan99@gmail.com>
2024-05-07 00:17:00 +0600

    service pattern and cache implemented

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,99 +9,50 @@ use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Enums\TransactionType;
 use Illuminate\Support\Facades\DB;
+use App\Services\TransactionService;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\Deposit\CreateRequest;
 use App\Http\Requests\Withdraw\CreateRequest as DepositRequest;
 
 class TransactionController extends Controller
 {
-    public function index(): View
+    public function index(TransactionService $transactionService): View
     {
-        $transactions = Transaction::where('user_id', auth()->id())->paginate(10);
-        $balance = auth()->user()->balance;
-        return view('transaction.index', compact('transactions', 'balance'));
+      $data =  $transactionService->getTransactionData();
+        return view('transaction.index', 
+        ['transactions' => $data['transactions'], 'balance' => $data['balance']]);
     }
 
-    public function depositTransction(): View
+    public function depositTransction(TransactionService $transactionService): View
     {
-        $depositTransactions = Transaction::where('transaction_type', TransactionType::Deposit->value)
-            ->where('user_id', auth()->id())
-            ->paginate(10);
-        $balance = auth()->user()->balance;
-        return view('transaction.deposit', compact('depositTransactions', 'balance'));
+        $data =  $transactionService->getDepositTransactionData();
+        return view('transaction.deposit', 
+        ['depositTransactions' => $data['depositTransactions'], 'balance' => $data['balance']]);
     }
 
-    public function addDeposit(CreateRequest $request): RedirectResponse
+    public function addDeposit(CreateRequest $request, TransactionService $transactionService)
     {
-        DB::transaction(function () use ($request) {
-            $user = auth()->user();
-            $user->balance += $request->amount;
-            $user->save();
-            Transaction::create([
-                'user_id' => $user->id,
-                'amount' => $request->amount,
-                'transaction_type' => TransactionType::Deposit,
-                'date' => now(),
-            ]);
-        });
-
-        return redirect()->route('transaction.deposit')->with('status', 'Deposit added successfully.');
+        if ($transactionService->addDeposit($request)) {
+            return redirect()->route('transaction.deposit')->with('status', 'Deposit added successfully.');
+        } else {
+            return redirect()->route('transaction.deposit')->with('status', 'Failed to add deposit.');
+        }
     }
 
-    public function withdrawTransction(): View
+    public function withdrawTransction(TransactionService $transactionService): View
     {
-        $withdrawTransactions = Transaction::where('transaction_type', TransactionType::Withdraw->value)
-            ->where('user_id', auth()->id())
-            ->paginate(10);
-        $balance = auth()->user()->balance;
-        return view('transaction.withdraw', compact('withdrawTransactions', 'balance'));
+       $data = $transactionService->getWithdrawTransactionData();
+        return view('transaction.withdraw', 
+        ['withdrawTransactions' => $data['withdrawTransactions'], 'balance' => $data['balance']]);
     }
 
-    public function addWithdraw(DepositRequest $request): RedirectResponse
+    public function addWithdraw(DepositRequest $request, TransactionService $transactionService): RedirectResponse
     {
-        $user = auth()->user();
-
-        try {
-            DB::transaction(function () use ($user, $request) {
-                $currentMonthWithdrawals = Transaction::where('user_id', $user->id)
-                    ->where('transaction_type', TransactionType::Withdraw)
-                    ->whereYear('date', now()->year)
-                    ->whereMonth('date', now()->month)
-                    ->sum('amount');
-
-                $todayWithdrawals = Transaction::where('user_id', $user->id)
-                    ->where('transaction_type', TransactionType::Withdraw)
-                    ->whereDate('date', today())
-                    ->sum('amount');
-
-                $currentMonthLimit = 5000 - $currentMonthWithdrawals;
-                $todayLimit = 1000 - $todayWithdrawals;
-
-                if (floatval($request->amount) <= $currentMonthLimit && floatval($request->amount) <= $todayLimit) {
-                    $fee = 0;
-                } else {
-                    $fee = floatval($request->amount) * (0.015 / 100);
-                }
-
-                if ($user->balance < $request->amount + $fee) {
-                    throw new \Exception('Insufficient balance.');
-                }
-
-                $user->balance -= $request->amount + $fee;
-                $user->save();
-
-                Transaction::create([
-                    'user_id' => $user->id,
-                    'transaction_type' => TransactionType::Withdraw,
-                    'amount' => $request->amount,
-                    'fee' => $fee,
-                    'date' => now(),
-                ]);
-            });
-
-            return redirect()->route('transaction.withdraw')->with('status', 'Withdrawal added successfully.');
-        } catch (\Exception $e) {
-            return redirect()->route('transaction.withdraw')->with('status', $e->getMessage());
+        $data = $transactionService->addWithdraw($request);
+        if ($data['success']) {
+            return redirect()->route('transaction.withdraw')->with('status', $data['message']);
+        } else {
+            return redirect()->route('transaction.withdraw')->with('status', $data['message']);
         }
     }
 }
